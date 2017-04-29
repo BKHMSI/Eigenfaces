046299a684269aabf23deec03e905c8bd9c28278
BKHMSI <badr@khamissi.com>
2017-04-29 18:10:59 +0200

    added steps 3 and 4, modified bash script

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def imshow(img, gray = True, title=''):
     plt.imshow(img, cmap='gray') if gray else plt.imshow(img)
     plt.show()
 
+# Step 1
 for face in xrange(num_faces):
     face_path = os.path.join(DATA_PATH, str(face+1).zfill(2))
     for i, file in enumerate(os.listdir(face_path)):
@@ -26,8 +27,17 @@ for face in xrange(num_faces):
         X_train[idx] = img
         Y_train[idx] = face
 
+# Step 2
 img_mean = np.mean(X_train, axis=0)
-imshow(img_mean, title='Mean Image')
-
 A = X_train - img_mean
+
+# Step 3
 U, S, V = np.linalg.svd(A)
+rank = np.sum(S > 1e-12)
+
+# Step 4
+Ur = U[:, :rank]
+X = Ur.T.dot(A)
+
+# Show Results
+imshow(img_mean, title='Mean Image')
