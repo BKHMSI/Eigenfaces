dd58490c238348a30c35424ef936052669b8fdd9
BKHMSI <badr@khamissi.com>
2017-05-12 22:17:07 +0200

    split data into training and validation set

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from preprocessing import facechop, histogram_eq
 
 
 DATA_PATH = '/Users/BAlKhamissi/Documents/Datasets/yalefaces'
-num_faces = 10
+num_faces = 15
 num_per_face = 11
 num_images = num_faces * num_per_face
 m = 70
@@ -16,11 +16,12 @@ n = 70
 img_dim = (m,n)
 input_dim = (num_images, img_dim[0], img_dim[1])
 X_train = np.zeros(input_dim, dtype='uint8')
-Y_train = np.zeros(num_images)
+X_val = np.zeros(input_dim, dtype='uint8')
 X_mean = np.zeros(img_dim, dtype='uint8')
+Y_train = np.zeros(num_images)
 
-EPSIOLON_ID = 1000
-EPSIOLON_DETECT = 2000
+EPSIOLON_ID = 3000
+EPSIOLON_DETECT = 2400
 
 def subplot(X, Y):
     fig = plt.figure()
@@ -41,8 +42,8 @@ def get_noise():
     return np.random.rand(m,n)
 
 def get_unknown_face():
-    # path = os.path.join(DATA_PATH, "12/subject12.normal")
-    img = plt.imread('images/unknown.jpg')
+    img = plt.imread('images/badr.jpg')
+    #img = plt.imread('images/unknown.jpg')
     return img
 
 def get_building():
@@ -51,17 +52,22 @@ def get_building():
 
 # Step 1
 def read_data():
+    t_idx = v_idx = 0
     for face in range(num_faces):
         face_path = os.path.join(DATA_PATH, str(face+1).zfill(2))
         for i, file in enumerate(os.listdir(face_path)):
             file_path = os.path.join(face_path, file)
             img = plt.imread(file_path, 0)
-            idx = face*num_per_face+i
             img = facechop(img)
             img = histogram_eq(img)
-            X_train[idx] = imresize(img, img_dim)
-            Y_train[idx] = face
-    return X_train, Y_train
+            if(i <= 9):
+                X_train[t_idx] = imresize(img, img_dim)
+                Y_train[t_idx] = face
+                t_idx = t_idx + 1
+            else: 
+                X_val[v_idx] = imresize(img, img_dim)
+                v_idx = v_idx + 1
+    return X_train, Y_train, X_val
 
 def train(X_train):
     # Unroll Images
@@ -93,13 +99,13 @@ def predict(img, X, Ur, X_mean, Y_train):
     print("e_f:", ep_a_x)
     if ep_a_x > EPSIOLON_DETECT:
         print("Not a face.")
-        return 0
+        return -2
     # end detect face
 
     D = X - x*np.ones((1,X.shape[0]))
     d = np.sqrt(np.diag(D.T.dot(D)))
     d_idx = np.argmin(d)
-    print("d: ", d[d_idx])
+    print("Distance: ", d[d_idx])
     if d[d_idx] < EPSIOLON_ID:
         print("This is Face #", Y_train[d_idx])
         return d_idx
@@ -116,23 +122,28 @@ def preprocess(img):
     return img
 
 
-X_train, Y_train = read_data()
+X_train, Y_train, X_val = read_data()
+
 X, Ur, X_mean = train(X_train)
 
 # test = X_train[56] # face
 # test = get_noise() 
-test = get_unknown_face()
-
-
-test = preprocess(test)
-print("Test Image: ", test.shape)
+# test = get_unknown_face()
 # test = get_building()
-pred = predict(test, X, Ur, X_mean, Y_train)
+try:
+    #test = preprocess(test)
+    pred = predict(X_val[i], X, Ur, X_mean, Y_train)
+except:
+    pred = -2
 
-if pred not in (-1, 0):
+if pred not in (-1, -2):
     test = test.reshape(img_dim)
     img = X_train[pred].reshape(img_dim)
     subplot(test,img)
+elif pred == -1:
+    imshow(test, title="Unknown Face")
+else:
+    imshow(test, title="Not a Face")
 
 # Show Results
 X_mean = X_mean.reshape(img_dim)
