b2f9a7198080f193786783cb592eb61ca46822e3
BKHMSI <badr@khamissi.com>
2017-05-12 02:51:08 +0200

    face identification is now working, detection needs some tweaking

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ from scipy.misc import imresize
 
 
 DATA_PATH = '/Users/BAlKhamissi/Documents/Datasets/yalefaces'
-num_faces = 15
+num_faces = 10
 num_per_face = 11
 num_images = num_faces * num_per_face
-img_dim = (60,80)
+m = 60
+n = 80
+img_dim = (m,n)
 input_dim = (num_images, img_dim[0], img_dim[1])
 X_train = np.zeros(input_dim, dtype='uint8')
 Y_train = np.zeros(num_images)
 X_mean = np.zeros(img_dim, dtype='uint8')
 
-EPSIOLON_ID = 10
+EPSIOLON_ID = 2900
 EPSIOLON_DETECT = 2
 
 def subplot(X, Y):
@@ -35,6 +37,21 @@ def imshow(img, gray = True, title=''):
     plt.imshow(img, cmap='gray') if gray else plt.imshow(img)
     plt.show()
 
+def get_noise():
+    return np.random.rand(m,n)
+
+def get_unknown_face():
+    path = os.path.join(DATA_PATH, "12/subject12.normal")
+    img = plt.imread(path, 0)
+    img = imresize(img, img_dim)
+    imshow(img)
+    return img
+
+def get_building():
+    img = cv.imread('building.jpg', 0)
+    img = imresize(img, img_dim)
+    return img
+
 # Step 1
 def read_data():
     for face in range(num_faces):
@@ -49,31 +66,30 @@ def read_data():
     return X_train, Y_train
 
 def train(X_train):
-    # global X_mean, X_train
     # Unroll Images
-    X_train = X_train.reshape(X_train.shape[0], -1)
-    # assert X_train.shape[0] == num_images
-    print("X_train.shape:", X_train.shape)
+    X_train = X_train.reshape(X_train.shape[0], -1).T
 
     # Step 2
-    X_mean = np.mean(X_train, axis=0)
-    A = X_train - X_mean
+    X_mean = np.mean(X_train, axis=1).reshape(X_train.shape[0], 1)
+
+    A = X_train - X_mean 
 
     # Step 3
-    U, S, V = np.linalg.svd(A.T)
+    U, S, V = np.linalg.svd(A)
     rank = np.sum(S > 1e-12)
-    print("U.shape:", U.shape)
 
     # Step 4
     Ur = U[:, :rank]
-    X = Ur.T.dot(A.T)
+    X = Ur.T.dot(A)
     return X, Ur, X_mean
 
 def predict(img, X, Ur, X_mean, Y_train):
-    img = img.reshape(-1)
+
+    img = img.reshape(1, -1).T
     a = img - X_mean
-    x = Ur.T.dot(a.T)
-    # Detect face
+    x = Ur.T.dot(a)
+
+    # detect face
     ap = Ur.dot(x)
     ep_a_x = np.linalg.norm(a - ap)
     print("e_f:", ep_a_x)
@@ -81,49 +97,32 @@ def predict(img, X, Ur, X_mean, Y_train):
         print("Not a face.")
         return 0
     # end detect face
-    e_face = np.linalg.norm(x - X, axis=0)
-    # e_face.reshape(-1)
-    print("e_face.shape:", e_face.shape)
 
-    for i, f in enumerate(e_face):
-        print(i, f)
-
-    # xx = x - X
-    # e_face = np.sqrt(xx.T.dot(xx))
-    idx = np.argmin(e_face)
-
-    print("Y_train.shape:", Y_train.shape)
-    print(idx)
-    print("Face #:", Y_train[idx])
-    return idx
-
-    # if e_face < EPSIOLON_ID:
-    #     D = X - x*np.ones((1,X_train.shape[0]))
-    #     d = np.sqrt(np.diag(D.T.dot(D)))
-    #     d_min, d_idx = np.min(d)
-    #     if d_min < EPSIOLON_DETECT:
-    #         print("This is Face #", Y_train[d_idx])
-    #     else:
-    #         print("Uknown Face")
-    # else:
-    #     print("Image is not a Face")
+    D = X - x*np.ones((1,X.shape[0]))
+    d = np.sqrt(np.diag(D.T.dot(D)))
+    d_idx = np.argmin(d)
+    print("d: ", d[d_idx])
+    if d[d_idx] < EPSIOLON_ID:
+        print("This is Face #", Y_train[d_idx])
+        return d_idx
+    else:
+        print("Uknown Face")
+        return -1
 
 
 X_train, Y_train = read_data()
 X, Ur, X_mean = train(X_train)
 
-#test = X_train[99] # face
-#test = np.random.rand*img_dim) # noise
-test = plt.imread('building.jpg') # not face
-test = plt.im
-test = imresize(test, img_dim)
-imshow(test)
+# test = X_train[56] # face
+# test = get_noise() 
+test = get_unknown_face()
+# test = get_building()
 pred = predict(test, X, Ur, X_mean, Y_train)
 
-test = test.reshape(img_dim)
-img = X_train[pred].reshape(img_dim)
-
-subplot(img,test)
+if pred not in (-1, 0):
+    test = test.reshape(img_dim)
+    img = X_train[pred].reshape(img_dim)
+    subplot(test,img)
 
 # Show Results
 X_mean = X_mean.reshape(img_dim)
